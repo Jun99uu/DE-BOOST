4bc169717a167e9c572c6e3bd0e3978321dc4825
Jun99uu_Lee <igun0423@naver.com>
2023-06-03 19:31:03 +0900

    💄 Style: add global style & font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import "@styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
