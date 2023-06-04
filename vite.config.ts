77dc5b349fa357fcb5f8fc9ef425a7292f4d215b
Jun99uu_Lee <igun0423@naver.com>
2023-06-04 12:53:22 +0900

    ➕ add dependency of svg-component

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,7 @@ export default defineConfig({
     react({
       jsxImportSource: "@emotion/react",
     }),
+    svgr(),
   ],
   resolve: {
     alias: [
