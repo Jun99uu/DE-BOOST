c7c5d836618ebc128391b97ab7269dd1608098f9
Jun99uu_Lee <igun0423@naver.com>
2023-06-12 23:04:58 +0900

    💄 푸터 여백 수정

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -1,17 +1,20 @@
 import Logo from "@/assets/Logo";
+import useMobile from "@/hooks/useMobile";
 import { colors, typography } from "@/styles/tokens";
 import styled from "@emotion/styled";
 import { useLocation } from "react-router-dom";
 
 const Footer = () => {
   const { pathname } = useLocation();
+  const isMobile = useMobile();
   const contents = [
     "DE:BOOST | 서울특별시 동작구 사당로 50 숭실대학교 그 어딘가",
     "특허번호 : 제2022-서울동작-1234 | Contact : DE:BOOST@email.com",
     "Copyright by DE:BOOST. All right reserved.",
   ];
+
   return (
-    <Container none={pathname === "/login"}>
+    <Container none={pathname === "/login"} isMobile={isMobile}>
       <Logo blue={true} />
       {contents.map((content) => (
         <Content key={content}>{content}</Content>
@@ -20,14 +23,15 @@ const Footer = () => {
   );
 };
 
-const Container = styled.div<{ none: boolean }>`
+const Container = styled.div<{ none: boolean; isMobile: boolean }>`
   width: 100%;
   display: ${(props) => (props.none ? "none" : "flex")};
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
   gap: 20px;
-  padding: 150px 50px 50px 50px;
+  padding: ${(props) =>
+    props.isMobile ? "150px 3rem 5rem 3rem" : "150px 50px 50px 50px"};
 `;
 
 const Content = styled.span`
